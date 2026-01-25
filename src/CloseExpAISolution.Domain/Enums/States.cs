be65fcb94b68a40b022969be436341a6a1ee85f0
khoindSE180124 <khoind2004@gmail.com>
2026-01-25 11:38:24 +0700

    feat: introduce OrderState, ProductState, and UserState enums

---
+++ src/CloseExpAISolution.Domain/Enums/States.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloseExpAISolution.Domain.Enums
+{
+    public enum OrderState
+    {
+        Pending,
+        PaidProcessing,
+        ReadyToShip,
+        DeliveredWaitingForConfirm,
+        Completed,
+        Canceled,
+        Refunded,
+        Failed
+    }
+
+    public enum ProductState
+    {
+       Verified,
+       Priced,
+       OutOfStock,
+       Available
+    }
+
+    public enum UserState
+    {
+        Active,
+        Inactive,
+        Banned
+    }
+}
