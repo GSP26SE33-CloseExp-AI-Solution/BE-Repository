55dd70f584e617c7ea6727b47cd3d5b6759dd8ea
khoindSE180124 <khoind2004@gmail.com>
2026-03-03 15:54:19 +0700

    fix(request): update password regex and add JsonConverter for RegistrationType

--- src/CloseExpAISolution.Application/DTOs/Request/RegisterRequest.cs
+++ src/CloseExpAISolution.Application/DTOs/Request/RegisterRequest.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using CloseExpAISolution.Domain.Enums;
 
 namespace CloseExpAISolution.Application.DTOs.Request;
@@ -19,11 +20,12 @@ public class RegisterRequest
 
     [Required(ErrorMessage = "Password is required")]
     [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters")]
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$",
         ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character")]
     public string Password { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Registration type is required")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public RegistrationType RegistrationType { get; set; }
 
     public Guid? SupermarketId { get; set; }
@@ -31,9 +33,14 @@ public class RegisterRequest
     public string? Position { get; set; }
 }
 
+/// <summary>
+/// Registration types available for public registration.
+/// Use string values: "Vendor" or "SupplierStaff"
+/// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum RegistrationType
 {
     Vendor = (int)RoleUser.Vendor,
-
+    
     SupplierStaff = (int)RoleUser.SupplierStaff
 }
