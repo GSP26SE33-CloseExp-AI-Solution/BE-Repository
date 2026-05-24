67e0c1e7b377851d9d7e6cc7b5d54ef25a1b0eb6
DuongPKSE180524 <66048953+ReshTheDragon@users.noreply.github.com>
2026-05-24 16:09:32 +0700

    feat: added Migration update new Entities

---
+++ src/CloseExpAISolution.Infrastructure/Migrations/20260522145226_Update PackagingStaff.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloseExpAISolution.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePackagingStaff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderPackaging_Users_UserId",
+                table: "OrderPackaging");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "OrderPackaging",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderPackaging_Users_UserId",
+                table: "OrderPackaging",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderPackaging_Users_UserId",
+                table: "OrderPackaging");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "OrderPackaging",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderPackaging_Users_UserId",
+                table: "OrderPackaging",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
